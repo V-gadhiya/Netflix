12e92cebd964a16009199ac1b733944868d9cd78
Vraj-2003 <vrajgadhiya84@gmail.com>
2024-02-01 14:20:26 +0530

    v

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,16 +1,18 @@
-
+import React from 'react';
 import MovieCard from "@/app/components/MovieCard";
-import styles from "@/app/style/common.module.css";
+import styles from "@/app/style/common.module.css"
 
 const Movie = async () => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const url = process.env.RAPID_KEY;
 
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+
+    const url = process.env.RAPID_KEY;
 
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'd8116f1042msha5a745329a9a22cp161773jsnaedcc173274b',
+            'X-RapidAPI-Key': 'c02fbc68bcmshbcf86db29a2c655p12007cjsn9adea8d51505',
             'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
         }
     };
@@ -18,28 +20,23 @@ const Movie = async () => {
     const res = await fetch(url, options);
     const data = await res.json();
     const main_data = data.titles;
-
+    console.log(main_data.jawSummary)
 
     return (
-        <div>
+        <>
             <section className={styles.movieSection}>
                 <div className={styles.container}>
-
-
-                    <h1>
-                        Series & Movie
-                    </h1>
+                    <h1>Series & Movie</h1>
                     <div className={styles.card_section}>
                         {
                             main_data.map((curElem) => {
-                                return <MovieCard key={curElem.id} {...curElem}/>
+                                return <MovieCard key={curElem.id} {...curElem} />
                             })
                         }
                     </div>
-
                 </div>
             </section>
-        </div>
+        </>
     );
 };
 
